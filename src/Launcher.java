d1c3ade161310501f97f563c75a49dbbb1bb843c
PaulBodin <pbodin@et.esiea.fr>
2021-11-29 15:15:32 +0100

    Fibo

--- src/Launcher.java
+++ src/Launcher.java
@@ -6,12 +6,32 @@ public class Launcher {
 
          try (Scanner scanner = new Scanner(System.in)) {
              while(true) {
-                 String a = scanner.nextLine();
-                 if (a.equals("quit")) {
+                 String mot = scanner.nextLine();
+                 if (mot.equals("quit")) {
                      break;
-                 } else {
-                     System.out.print("Unknown command\n");
                  }
+                 if(mot.equals("fibo"))
+                 {
+                     System.out.print( "Veuillez saisir un premier entier : " );
+                     int a = scanner.nextInt();
+
+                     int nbr1=0, nbr2=1, nbr3=0, i, count=a+1;
+                     //afficher 0 et 1
+                     System.out.print("F(0) : "+nbr1);
+                     System.out.print("\nF(1) : "+nbr2);
+                     //La boucle commence par 2 car 0 et 1 sont deja affiches
+                     for(i=2; i<count; ++i)
+                     {
+                         nbr3 = nbr1 + nbr2;
+                         nbr1 = nbr2;
+                         nbr2 = nbr3;
+                     }
+                     System.out.print("\nF("+a+") : "+nbr3);
+
+                     break;
+                 }
+
+                 System.out.print("Unknow command\n");
              }
          }
     }
