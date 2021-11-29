0978bd85dbd24f0283c6a49dee7101e0b623b6d8
PaulBodin <pbodin@et.esiea.fr>
2021-11-29 14:55:57 +0100

    Boucle

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,18 +4,16 @@ public class Launcher {
 
     public static void main(String[] args) {
 
-        try ( Scanner scanner = new Scanner( System.in ) ) {
-            String a = scanner.nextLine();
-            if (a.equals("quit"))
-            {
-
-            }
-            else
-            {
-                System.out.print( "Unknown command");
-            }
-        }
-
+         try (Scanner scanner = new Scanner(System.in)) {
+             while(true) {
+                 String a = scanner.nextLine();
+                 if (a.equals("quit")) {
+                     break;
+                 } else {
+                     System.out.print("Unknown command\n");
+                 }
+             }
+         }
     }
-
 }
+
